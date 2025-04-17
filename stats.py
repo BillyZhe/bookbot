190f9036fcfa500d398db0838fa346728559a732
BillyZhe <zheman615@gmail.com>
2025-04-17 04:32:16 -0500

    updating main.py to work

--- stats.py
+++ stats.py
@@ -2,4 +2,14 @@ def get_word_count(book):
     words = book.split()
     return len(words)
 
+def get_letter_count(book):
+    count_dict = {}
+    words = list(book)
+    for char in words:
+        low = char.lower()
+        if count_dict.get(low) == None:
+            count_dict.update({low: 1})
+        else:
+            count_dict[low] += 1
+    return count_dict  
 
