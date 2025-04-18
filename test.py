eaf02b48f2bfb281ad7b6ddb13f4ad7603c6f24c
BillyZhe <zheman615@gmail.com>
2025-04-18 06:35:05 -0500

    trying new change to code

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ def get_letter_count(book):
     return count_dict 
 def get_order(dic):
     order_dic = {}
-    order_dic = get_letter_count.sort(reverse = True, key= get_order)
+    order_dic = get_letter_count(reverse = True, key = get_order)
     return order_dic
 def main():
     with open("books/frankenstein.txt") as file:
         file_contents = file.read()
         get_letter = get_letter_count(file_contents)
-        for_order = get_order
-    print(get_order)
+        for_order = get_order(get_letter_count)
+    print(for_order)
 main()
 
