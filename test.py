bd57ccdf498fd3976dc2e24ddf3d9f26d06f2490
BillyZhe <zheman615@gmail.com>
2025-04-18 07:33:54 -0500

    still working to get the code to work

--- test.py
+++ test.py
@@ -4,15 +4,13 @@ def get_letter_count(book):
         low = char.lower()
         count_dict[low] = count_dict.get(low,0) + 1
     return count_dict 
-def get_order(dic):
-    order_dic = {}
-    order_dic = get_letter_count(reverse = True, key = get_order)
-    return order_dic
+def get_order(dict):
+    return dict["num"]
 def main():
     with open("books/frankenstein.txt") as file:
         file_contents = file.read()
         get_letter = get_letter_count(file_contents)
-        for_order = get_order(get_letter_count)
+        for_order = get_order
     print(for_order)
 main()
 
