0dfe21ca158ada6458b36059b56b8e3b8350b596
BillyZhe <zheman615@gmail.com>
2025-04-27 07:13:44 -0500

    Trying my best

--- testdict.py
+++ testdict.py
@@ -1,3 +1,26 @@
 class Book:
-    def __init__(self,title,author):
+    def __init__(self, title, author):
         self.title = title
+        self.author = author 
+
+
+class Library:
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+
+    def add_book(self, book):
+        if isinstance(book, list):
+            self.books.extend(book)
+        else:
+            self.books.append(book)
+
+    def remove_book(self, book):
+        print(self.books)
+        for word in book:
+            word.lower()
+            if word in self.books():
+                self.books.remove(book)
+
+    def search_books(self, search_string):
+        pass
