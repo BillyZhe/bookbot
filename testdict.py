325de42dedf1473303d26366c2d26442f9ceeac7
BillyZhe <zheman615@gmail.com>
2025-06-12 05:05:44 -0500

    trying so hard

--- testdict.py
+++ testdict.py
@@ -1,26 +1,4 @@
-class Book:
-    def __init__(self, title, author):
-        self.title = title
-        self.author = author 
+why is this so hard 
 
 
-class Library:
-    def __init__(self, name):
-        self.name = name
-        self.books = []
-
-    def add_book(self, book):
-        if isinstance(book, list):
-            self.books.extend(book)
-        else:
-            self.books.append(book)
-
-    def remove_book(self, book):
-        print(self.books)
-        for word in book:
-            word.lower()
-            if word in self.books():
-                self.books.remove(book)
-
-    def search_books(self, search_string):
-        vi
+    
